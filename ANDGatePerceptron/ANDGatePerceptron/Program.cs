89725ee3647bc8018e6369002dd2d68f4cbb3829
Edan Saedi <edsaedi@gmail.com>
2022-01-26 18:19:07 -0800

    I started the project.

---
+++ ANDGatePerceptron/ANDGatePerceptron/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ANDGatePerceptron
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
